1b4fc147449745d48e3101396928eda456f1e224
Chitroopa Manikkavasagam and Jesse Hoare Evans <jesse@>
2017-05-03 16:45:19 -0700

    correct train specs

--- lib/train.rb
+++ lib/train.rb
@@ -9,4 +9,19 @@ class Train
     @arrival_location = attributes.fetch(:arrival_location)
   end
 
+  def self.all
+    returned_trains = DB.exec("SELECT * FROM trains")
+    trains = []
+      returned_trains.each() do |train|
+        name = train.fetch("name")
+        id = train.fetch("id").to_i()
+        departure_time = attributes.fetch('departure_time')
+        departure_location = attributes.fetch('departure_location')
+        arrival_time = attributes.fetch('arrival_time')
+        arrival_location = attributes.fetch('arrival_location')
+        trains.push(Train.new({:id => id, :name => name, :departure_time => departure_time, :departure_location=> departure_location, :arrival_time => arrival_time, :arrival_location=> arrival_location}))
+      end
+      trains
+    end
+    
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -11,5 +11,55 @@ describe (Train) do
       expect(test_train.arrival_location()).to(eq('Portland'))
     end
   end
-  
+
+  describe(".all") do
+    it("starts off with no trains") do
+      expect(Train.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("lets you save trains to the database") do
+      test_train = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      test_train.save()
+      expect(Train.all()).to(eq([test_train]))
+    end
+  end
+
+  describe("#==") do
+    it("is the same train if it has the same name") do
+      train1 = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      train2 = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      expect(train1).to(eq(train2))
+    end
+  end
+
+  describe(".find") do
+    it("returns a list by its ID") do
+      test_train = Train.new({:name => 'Seattle express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      test_train.save()
+      test_train2 = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      expect(Train.find(test_train2.id())).to(eq([test_train2]))
+    end
+  end
+
+  describe("#update") do
+    it("lets you update trains in the database") do
+      train = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      train.save()
+      train.update({:name => "Seattle Express"})
+      expect(train.name()).to(eq("Seattle Express"))
+    end
+  end
+
+  describe("#delete") do
+    it("lets you delete a train from the database") do
+      train = Train.new({:name => 'Portland express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      train.save()
+      train2 = Train.new({:name => 'Seattle express', :id => nil, :departure_time => '10:00:00', :departure_location => "Seattle", :arrival_time => '11:00:00', :arrival_location => "Portland"} )
+      train2.save()
+      train.delete()
+      expect(Train.all()).to(eq([train2]))
+    end
+  end
 end
