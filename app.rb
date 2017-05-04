a48ce4899fd4725a908ead424799f878a52888a0
Chitroopa Manikkavasagam and Jesse Hoare Evans <chitroopa@>
2017-05-04 15:45:25 -0700

    add rider schedule pages

--- app.rb
+++ app.rb
@@ -19,7 +19,22 @@ get('/system_operator') do
   erb(:train_system)
 end
 
+get('/rider') do
+  @cities = City.all()
+  erb(:rider)
+end
 
+post('/rider/trains') do
+  city_id = params.fetch('city_id').to_i()
+  @city = City.find(city_id)
+  erb(:rider_train)
+end
+
+get('/rider/trains/:id') do
+  @train = Train.find(params.fetch("id").to_i())
+  @cities = City.all()
+  erb(:rider_train_info)
+end
 get('/trains/:id') do
   @train = Train.find(params.fetch("id").to_i())
   @cities = City.all()
@@ -64,8 +79,34 @@ delete('/cities/:id') do
   erb(:train_system)
 end
 
+get('/city/new') do
+  erb(:city_form)
+end
+
+post('/city/new') do
+  name = params.fetch("name")
+  new_city = City.new({:name => name, :id => nil})
+  new_city.save()
+  @trains = Train.all()
+  @cities = City.all()
+  erb(:train_system)
+end
+
 get('/train/new') do
   erb(:train_form)
+end
+
+post('/train/new') do
+  name = params.fetch("name")
+  departure_time = params.fetch("departure_time")
+  departure_location = params.fetch("departure_location")
+  arrival_time = params.fetch("arrival_time")
+  arrival_location = params.fetch("arrival_location")
+  new_train = Train.new({:name => name, :departure_time => departure_time, :departure_location => departure_location, :arrival_time => arrival_time, :arrival_location => arrival_location, :id => nil})
+  new_train.save()
+  @trains = Train.all()
+  @cities = City.all()
+  erb(:train_system)
 end
 
 get('/train') do
